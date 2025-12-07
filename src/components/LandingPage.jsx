90140286eba26184f2eb9a3861dc36bbde900f3b
Jordain <jordainjoyce@hotmail.com>
2025-12-07 17:51:39 -0500

    chore: Price update

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -92,7 +92,7 @@ const LandingPage = () => {
               },
               {
                 duration: "30",
-                price: "80.00",
+                price: "75.00",
                 bookingUrl:
                   "https://simonejoycermtltd.janeapp.com/#/staff_member/1/treatment/4",
               },
